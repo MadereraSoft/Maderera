71f749908f2494045d031b1fc50bc33deef5a586
mmc01 <a20097102@pucp.edu.pe>
2014-01-12 23:23:44 -0500

    Commit Marce2

---
+++ src/pe/edu/pucp/lumberjack/controller/ultimaMMC.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.pucp.lumberjack.controller;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class ultimaMMC {
+    
+}
